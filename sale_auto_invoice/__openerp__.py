2c19382991a5df17929833c01d2fba7fe5053862
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2015-12-21 11:19:03 +0100

    [ADD] Sales Auto Invoice module

---
+++ sale_auto_invoice/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 ICTSTUDIO (<http://www.ictstudio.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Auto Invoice',
+    'version': '1.0',
+    'category': 'Sales & Invoicing',
+    'description': """Automatic creation of invoice on transfer of delivery. Works with
+    Order policy: 'manual' (On Demand) and 'picking' (On Delivery Order)
+    """,
+    'author': 'ICTSTUDIO, André Schenkels',
+    'license': 'AGPL-3',
+    'website': 'http://www.ictstudio.eu',
+    'depends': [
+        'base',
+        'sale',
+        'account',
+        'stock',
+    ],
+    'data': [
+        'view/sale_view.xml',
+    ],
+}
