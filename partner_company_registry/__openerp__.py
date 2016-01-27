63c3c4a4795e1dd4d06b9a98bcd291f80838cb3a
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-01-27 22:13:25 +0100

    [FIX]

--- partner_company_registry/__openerp__.py
+++ partner_company_registry/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner Company Registry',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'CRM',
     'description': """Partner Company Registry
     Adds a Partner Registry field to the partner object.
