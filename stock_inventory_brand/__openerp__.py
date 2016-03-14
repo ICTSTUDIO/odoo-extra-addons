8d3fa5b77cd0be053d2508218b162c759b7a9788
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-03-14 17:05:10 +0100

    [ADD] stock invenotry specific brand module

---
+++ stock_inventory_brand/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 ICTSTUDIO (<http://www.ictstudio.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Stock Inventory for specific product brand',
+    'version': '8.0.1.0.0',
+    'website': 'http://www.ictstudio.eu',
+    'category': 'Product',
+    'summary': 'Provide Stock inventory for one specific brand',
+    'description': """
+Stock Inventory Product Brand
+=============================
+- On the stock inventory form make the brand selectable
+""",
+    'author': 'ICTSTUDIO, André Schenkels',
+    'depends': [
+        'stock',
+        'product_brand'
+    ],
+    'data': [
+        'views/stock_inventory.xml',
+    ],
+}
+
