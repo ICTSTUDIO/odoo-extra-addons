a545604027e85e5c39d33c4b3e7bfe1dc34d569a
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-06-28 10:34:56 +0200

    [ADD] Stock Move Location - Stock Move FIltering

---
+++ stock_move_location/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 ICTSTUDIO (<http://www.ictstudio.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Stock Move Sort On Location',
+    'version': '8.0.0.0.2',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'category': 'Stock Management',
+    'website': 'https://www.odoo.com',
+    'depends': ['product_stock_location'],
+    'demo': [],
+    'summary': "Show Move sort on location",
+    'data': [
+        'views/stock_move.xml'
+    ],
+}
+
