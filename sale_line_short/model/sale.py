cab93b0b805d44658e7a29dbdbd0e3e255df9deb
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-08-30 10:35:26 +0200

    [CHANGE] Small Changes

--- sale_line_short/model/sale.py
+++ sale_line_short/model/sale.py
@@ -18,9 +18,11 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api, _
 import logging
+from openerp import models, fields, api, _
+
 _logger = logging.getLogger(__name__)
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
