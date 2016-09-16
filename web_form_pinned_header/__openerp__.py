927bb3e44f535f09f7e77061c5fd3e40fa42925c
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-09-16 15:50:49 +0200

    [ADD] web form pinned header: module for pinning the header of a form when scrolling down.

---
+++ web_form_pinned_header/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Web Form Pinned Header',
+    'version': '8.0.1.0.1',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'category': 'Web',
+    'summary': 'Sheet width relative to screen size',
+    'license': 'AGPL-3',
+    'category': 'Web',
+    'depends': [
+        'web',
+        ],
+    'data': [
+        'views/web_form_pinned_header.xml',
+        ],
+}
