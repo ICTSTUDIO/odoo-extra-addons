11ffc7d6376912a1f585aefa36e4a508b44ff5a6
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-09-16 14:31:16 +0200

    [IMP] Product Prices modules with ability to remove special created prices

--- product_pricelist_prices/models/product_pricelist.py
+++ product_pricelist_prices/models/product_pricelist.py
@@ -1,22 +1,6 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2016 ICTSTUDIO (<http://www.ictstudio.eu>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import logging
 
@@ -33,14 +17,21 @@ class product_pricelist(models.Model):
             inverse="_set_product_price",
             string="Price"
     )
+    product_price_manual = fields.Boolean(
+            compute="_get_price_manual",
+            # inverse="_set_price_manual",
+            string="Manual Price",
+
+    )
     product_id = fields.Integer(
             #comodel_name="product.product",
             compute="_get_product_price",
             string="product_id"
     )
 
-    @api.one
-    def _get_product_price(self):
+    @api.model
+    def _get_product_id(self):
+        product_id = False
         if self.env.context.get('product_template_id', False):
             product_tmpl = self.env['product.template'].browse(
                     [self.env.context.get('product_template_id')]
@@ -52,22 +43,60 @@ class product_pricelist(models.Model):
                     product = product_tmpl_id.product_variant_ids[0]
                     if product:
                         product_id = product.id
-            if not product_id:
-                return False
         elif self.env.context.get('product_id', False):
             product_id = self.env.context.get('product_id')
-        else: 
-            return False
 
-        self.product_price = self.price_get(
-                product_id, 1).get(self.id, 0.0
-                                   )
-        self.product_id = product_id
+        return product_id
+
+    @api.one
+    def _get_price_manual(self):
+        product_id = self._get_product_id()
+
+        version = self.get_active_pricelist_version()
+        if version:
+            if self._context.get('product_id') and \
+                    self.env['product.pricelist.item'].search(
+                            [
+                                ('price_version_id', '=', version.id),
+                                ('product_id', '=', self._context.get('product_id'))
+                            ]
+                    ):
+                self.product_price_manual = True
+            if self._context.get('product_template_id') and \
+                    self.env['product.pricelist.item'].search(
+                            [
+                                ('price_version_id', '=', version.id),
+                                ('product_tmpl_id', '=', self._context.get('product_template_id'))
+                            ]
+                    ):
+                self.product_price_manual = True
+
+    @api.one
+    def _get_product_price(self):
+        product_id = self._get_product_id()
+        if product_id:
+            self.product_price = self.price_get(
+                    product_id, 1).get(self.id, 0.0
+            )
+            self.product_id = product_id
 
     def _set_product_price(self):
-            # Real change takes place in price_set after inverse
-            # method of pricelists object on product_template
-            _logger.debug("Set Price: %s", self.product_price)
+        # Real change takes place in price_set after inverse
+        # method of pricelists object on product_template
+        _logger.debug("Set Price: %s", self.product_price)
+
+
+
+    @api.one
+    def remove_price_manual(self):
+        # Real change takes place in price_set after inverse
+        # method of pricelists object on product_template
+        if self._context.get('product_template_id'):
+            self.price_remove(self._context.get('product_template_id'))
+        if self._context.get('product_id'):
+            product = self.env['product.product'].browse(self._context.get('product_id'))
+            self.price_remove(product.product_tmpl_id.id)
+
 
     def price_set(self, product_template, new_price):
         """
@@ -112,13 +141,32 @@ class product_pricelist(models.Model):
                                 'base': product_price_type_ids and product_price_type_ids[0].id,
                                 'sequence': 1,
                                 'name': product_template.name,
-                                'product_id': product_template.id,
+                                'product_tmpl_id': product_template.id,
                                 'price_version_id': version.id,
                                 'price_surcharge': new_price,
                                 'price_discount': -1
                             }
                     )
             return True
+
+    def price_remove(self, product_template_id):
+
+        version = self.get_active_pricelist_version()
+        if not version:
+            return False
+
+        items = self.env['product.pricelist.item'].search(
+                    [
+                        ('product_tmpl_id', '=', product_template_id),
+                        ('price_version_id', '=', version.id)
+                    ]
+        )
+        _logger.debug("Items: %s", items)
+        for item in items:
+            _logger.debug("Remove Item: %s", item)
+            item.unlink()
+        return True
+
 
 
     def get_active_pricelist_version(self):
