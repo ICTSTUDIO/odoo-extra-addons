226a5921c1b9abd1cc1a495a9f818b902db549b5
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-01-27 14:30:03 +0100

    [IMP] Module to show and search Sales Team on Outgoing Pickings [ADD] Sale Order on Picking stored module

--- stock_sale_order_stored/__openerp__.py
+++ stock_sale_order_stored/__openerp__.py
@@ -3,18 +3,17 @@
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Picking Sale Order Salesteam',
-    'version': '8.0.1.0.2',
+    'name': 'Sale Order Picking stored and indexed',
+    'version': '8.0.1.0.1',
     'category': 'Sales & Invoicing',
-    'summary': """Sales Team on Picking related to Order
+    'summary': """Sale Order on Picking stored and indexed
     """,
     'author': 'ICTSTUDIO, André Schenkels',
     'license': 'AGPL-3',
     'website': 'http://www.ictstudio.eu',
     'depends': [
-        'stock_sale_order_stored'
+        'sale_stock'
     ],
     'data': [
-        'views/stock_picking.xml',
     ],
 }
--- stock_sale_salesteam/models/stock_picking.py
+++ stock_sale_salesteam/models/stock_picking.py
@@ -12,40 +12,14 @@ class StockPicking(models.Model):
 
     section_id = fields.Many2one(
             compute='_get_section_id',
-            search='_search_section_id',
             comodel_name='crm.case.section',
-            string='Sales Team'
+            string='Sales Team',
+            store=True,
+            index=True
     )
 
     @api.one
-    @api.depends('group_id')
+    @api.depends('sale_id')
     def _get_section_id(self):
         if self.sale_id and self.sale_id.section_id:
             self.section_id = self.sale_id.section_id
-
-
-    def _search_section_id(self, operator, value):
-        res = []
-        picking_ids = []
-        assert operator in ('=', 'in'), 'Invalid domain operator'
-        assert isinstance(value, (float, int)), 'Invalid domain right operand'
-
-        sale_orders = self.env['sale.order'].search(
-                [
-                    ('section_id', operator, value),
-                    ('procurement_group_id', '!=', False)
-                ]
-        )
-        group_ids = []
-        for order in sale_orders:
-            group_ids.append(order.procurement_group_id.id)
-        _logger.debug('GroupIds: %s', group_ids)
-
-        stock_pickings = self.search([
-            ('group_id', 'in', group_ids)
-        ])
-        for picking in stock_pickings:
-            picking_ids.append(picking.id)
-
-        res.append(('id', 'in', picking_ids))
-        return res
