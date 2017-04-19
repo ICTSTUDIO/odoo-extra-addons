3d139966f660a0fcd41447e11af0f43a17ca100c
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-04-19 13:49:41 +0200

    [UPG] Module: product_stock_move_qty migrate to ODOO 9

---
+++ product_stock_move_qty/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    'name': 'Product Stock Move Qty',
+    'version': '8.0.1.2.1',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'license': 'AGPL-3',
+    'website': 'http://www.ictstudio.eu',
+    'category': 'Stock Management',
+    'summary': 'Show Product Stock Moves Qty for Location/Warehouse',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_move_location.xml',
+        'views/product_template.xml',
+        'views/product_product.xml',
+        'security/ir.model.access.csv'
+    ],
+}
+
