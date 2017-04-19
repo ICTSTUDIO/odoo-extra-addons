1972af392111b25f72823ebd4b6e2f032eefa2a3
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-04-19 15:12:17 +0200

    [ADD] Simple Partner Sequence module for ODOO 9

---
+++ simple_partner_sequence/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Simple Partner Sequence',
+    'version': '9.0.0.0.1',
+    'category': 'Partner Management',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'license': 'AGPL-3',
+    'website': 'http://www.ictstudio.eu',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_sequence.xml',
+        'data/ir_sequence_res_partner.xml',
+        'security/ir.model.access.csv'
+    ],
+}
