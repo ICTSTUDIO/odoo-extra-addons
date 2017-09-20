9794af9332410d0501c74b44d7a6ccd3ca9075cc
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-09-21 01:10:05 +0200

    [IMP] Spli

--- product_sync_external_id/models/__init__.py
+++ product_sync_external_id/models/__init__.py
@@ -2,18 +2,5 @@
 # Copyright© 2016-2017 ICTSTUDIO <http://www.ictstudio.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from . import magento_configure
-from . import magento_store
-from . import link
-from . import ext_order_matrix
-from . import sale_order
-from . import invoice
-from . import stock_picking
 from . import product_product
-from . import product_template
-from . import product_product_magento_store
-from . import magento_product
-from . import magento_product_type
-from . import stock_move
-from . import link_attribute
 
