4bbe35e95f07fea716ae7499dc26f9ef1abc167a
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-09-22 13:06:24 +0200

    [FIX] sale_cancel_refund advanced reconcile (also for other lines with account.reconcile = True

--- sale_cancel_refund/models/sale_order.py
+++ sale_cancel_refund/models/sale_order.py
@@ -12,17 +12,6 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # @api.model
-    # def _get_refund_journal(self):
-    #     journal = self.env['account.journal'].search(
-    #             [
-    #                 ('type', '=', 'sale_refund'),
-    #                 ('company_id', 'child_of', self.env.user.company_id.id)
-    #             ],
-    #             limit=1
-    #     )
-    #     return journal and journal[0] or False
-
     @api.multi
     def action_create_refund(self):
         refunds = self.env['account.invoice']
@@ -33,7 +22,6 @@ class SaleOrder(models.Model):
                     date = fields.Date.today()
                     period = inv.period_id and inv.period_id.id or False
                     description = inv.internal_number and 'Credit: %s' % inv.internal_number or 'Credit'
-                    #journal = self._get_refund_journal() or inv.journal_id
 
                     refund = inv.refund(date, period, description)
 
@@ -45,25 +33,25 @@ class SaleOrder(models.Model):
                     )
                     refund.button_compute()
                     refund.signal_workflow('invoice_open')
-                    self.reconcile_invoice_refund(inv, refund)
+                    if refund.state == 'open':
+                        self.reconcile_invoice_refund(inv, refund)
                     refunds += refund
                 else:
                     inv.action_cancel()
-            return refunds
+        return refunds
 
     @api.model
     def reconcile_invoice_refund(self, invoice, refund):
         movelines = invoice.move_id.line_id
         to_reconcile_ids = {}
         for line in movelines:
-            print line.product_id
-            if line.account_id.id == invoice.account_id.id:
+            if line.account_id.reconcile:
                 to_reconcile_ids.setdefault(line.account_id.id, []).append(line.id)
             if line.reconcile_id:
                 line.reconcile_id.unlink()
 
         for refundline in refund.move_id.line_id:
-            if refundline.account_id.id == invoice.account_id.id:
+            if refundline.account_id.reconcile:
                 to_reconcile_ids[refundline.account_id.id].append(refundline.id)
 
         for account in to_reconcile_ids:
@@ -114,7 +102,6 @@ class SaleOrder(models.Model):
     @api.multi
     def action_cancel(self):
         self.ensure_one()
-        # ctx = dict(self._context, refund_invoice=True, cancel_pickings=True)
 
         if self.order_policy == 'manual' and len(self.invoice_ids) == 1:
             self.action_advanced_cancel()
