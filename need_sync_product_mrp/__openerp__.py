8eb7bbecb5e49aeeed930ac9b5067ec718469f40
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-09-28 14:38:01 +0200

    [ADD] product main group / top level group

--- need_sync_product_mrp/__openerp__.py
+++ need_sync_product_mrp/__openerp__.py
@@ -3,7 +3,7 @@
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Need Synchronization Product MRP Module',
-    'version': '8.0.0.0.5',
+    'version': '8.0.0.0.7',
     'category': 'Stock',
     'author': 'ICTSTUDIO, André Schenkels',
     'website': 'http://www.ictstudio.eu',
