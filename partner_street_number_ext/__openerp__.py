dbcc384eb1b67f1a5d5193955dc132494aecfb86
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-10-04 13:52:12 +0200

    [ADD] Partner Street Extension module

---
+++ partner_street_number_ext/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2017 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Street name and number extension",
+    "summary": "Adding a housenumber extension field",
+    "version": "8.0.1.0.1",
+    "author": "ICTSTUDIO, André Schenkels",
+    "website": "https://github.com/oca/partner-contact",
+    "category": 'Tools',
+    "depends": [
+        'partner_street_number'
+        ],
+    "data": [
+        'views/res_partner.xml',
+        ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
