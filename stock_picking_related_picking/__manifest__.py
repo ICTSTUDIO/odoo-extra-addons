da35289d0a069ef029c10edd5c99c774e218350f
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-11-23 13:16:43 +0100

    [UPG] upgrade module to v10

---
+++ stock_picking_related_picking/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    'name': 'Stock Picking Related Pickings',
+    'version': '10.0.1.1.4',
+    'category': 'Stock',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'website': 'http://www.ictstudio.eu',
+    'license': 'AGPL-3',
+    'summary': 'Show related pickings on stock picking',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml'
+    ],
+}
---
+++ stock_picking_related_picking/models/stock_picking.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    related_pickings = fields.Many2many(
+        comodel_name='stock.picking',
+        compute="get_related_pickings",
+        string="Related Pickings"
+    )
+    related_pickings_name = fields.Char(
+         string='Related Pickings Name',
+         compute="get_related_pickings",
+    )
+
+    @api.model
+    def _get_related_pickings(self):
+        related_pickings = self
+        if self.group_id:
+            for group in self.group_id:
+                related_pickings = related_pickings | self.search(
+                    [
+                        ('group_id', '=', group.id)
+                    ]
+                )
+        else:
+            orig_pickings = self.mapped('move_lines.move_orig_ids.picking_id')
+            dest_pickings = self.mapped('move_lines.move_dest_id.picking_id')
+            related_pickings = orig_pickings | dest_pickings
+
+        return related_pickings - self
+
+    @api.model
+    def _get_related_pickings_name(self):
+        return ', '.join([rp.name for rp in self.related_pickings])
+
+    @api.multi
+    @api.depends('move_lines', 'group_id')
+    def get_related_pickings(self):
+        for rec in self:
+            rec.related_pickings = rec._get_related_pickings()
+            rec.related_pickings_name = rec._get_related_pickings_name()
+
+
+
+
