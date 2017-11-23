08145e230a2be03cca89a169ac1d6e01e4481486
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-11-23 13:09:59 +0100

    [IMP] adjustments to related pickings calc, fixing some minor issues

--- stock_picking_related_picking/__openerp__.py
+++ stock_picking_related_picking/__openerp__.py
@@ -3,7 +3,7 @@
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Stock Picking Related Pickings',
-    'version': '8.0.1.1.3',
+    'version': '8.0.1.1.4',
     'category': 'Stock',
     'author': 'ICTSTUDIO, André Schenkels',
     'website': 'http://www.ictstudio.eu',
--- stock_picking_related_picking/models/stock_picking.py
+++ stock_picking_related_picking/models/stock_picking.py
@@ -17,34 +17,37 @@ class StockPicking(models.Model):
     )
     related_pickings_name = fields.Char(
          string='Related Pickings Name',
-         compute='_get_related_pickings_name',
+         compute="get_related_pickings",
     )
+
+    @api.model
+    def _get_related_pickings(self):
+        related_pickings = self
+        if self.group_id:
+            for group in self.group_id:
+                related_pickings = related_pickings | self.search(
+                    [
+                        ('group_id', '=', group.id)
+                    ]
+                )
+        else:
+            orig_pickings = self.mapped('move_lines.move_orig_ids.picking_id')
+            dest_pickings = self.mapped('move_lines.move_dest_id.picking_id')
+            related_pickings = orig_pickings | dest_pickings
+
+        return related_pickings - self
+
+    @api.model
+    def _get_related_pickings_name(self):
+        return ', '.join([rp.name for rp in self.related_pickings])
 
     @api.multi
     @api.depends('move_lines', 'group_id')
     def get_related_pickings(self):
         for rec in self:
-            related_pickings = rec
-            if rec.group_id:
-                for group in rec.group_id:
-                    related_pickings = related_pickings | self.search(
-                        [
-                            ('group_id', '=', group.id)
-                        ]
-                    )
-            else:
-                orig_pickings = rec.mapped('move_lines.move_orig_ids.picking_id')
-                dest_pickings = rec.mapped('move_lines.move_dest_id.picking_id')
-                related_pickings = orig_pickings | dest_pickings
+            rec.related_pickings = rec._get_related_pickings()
+            rec.related_pickings_name = rec._get_related_pickings_name()
 
-            _logger.debug("Related Pickings: %s", related_pickings)
-            related_pickings = related_pickings - self
-            rec.related_pickings = related_pickings
 
-    @api.multi
-    @api.depends('related_pickings')
-    def _get_related_pickings_name(self):
-        for rec in self:
-            rec.related_pickings_name = ', '.join([rp.name for rp in rec.related_pickings])
 
 
