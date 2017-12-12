628f4acc66034ecf1fd0adf6b1af341e8b3ebb87
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2017-12-12 09:26:55 +0100

    [ADD] Product Prices module for v10

---
+++ product_pricelist_prices/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Products Pricelist Prices',
+    'version': '10.0.1.1.4',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'website': 'http://www.ictstudio.eu',
+    'license': 'AGPL-3',
+    'category': 'Sales Management',
+    'depends': ['product'],
+    'demo': [],
+    'summary': "Show Price for Available Pricelists",
+    'data': [
+        'views/product_pricelist.xml',
+        'views/product_product.xml',
+        'views/product_template.xml',
+    ],
+}
+
