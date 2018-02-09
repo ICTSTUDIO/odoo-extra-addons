4d41f205c4ee5a7022c1eba2804ef435c1033b8e
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2018-02-09 17:10:10 +0100

    [IMP] payment link

--- payment_sale_order/__openerp__.py
+++ payment_sale_order/__openerp__.py
@@ -3,7 +3,7 @@
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Payment Url - Sale Order',
-    'version': '8.0.0.0.1',
+    'version': '8.0.0.0.2',
     'category': 'Stock',
     'author': 'ICTSTUDIO | André Schenkels',
     'website': 'http://www.ictstudio.eu',
