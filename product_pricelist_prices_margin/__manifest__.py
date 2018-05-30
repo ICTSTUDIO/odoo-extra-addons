18b6c4d5c96623c763fd99639b6a37721d612ba4
André Schenkels <a.schenkels@ictstudio.eu>
2018-05-30 11:35:40 +0200

    [ADD] Product Prices Margin module (show margin behind each pricelist price on the product form]

---
+++ product_pricelist_prices_margin/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Products Pricelist Prices - Margin Extension',
+    'version': '10.0.0.0.1',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'website': 'http://www.ictstudio.eu',
+    'license': 'AGPL-3',
+    'category': 'Sales Management',
+    'depends': ['product_pricelist_prices'],
+    'demo': [],
+    'summary': "Show Margin for Available Pricelists Prices",
+    'data': [
+        'views/product_product.xml',
+        'views/product_template.xml',
+    ],
+}
+
---
+++ product_pricelist_prices_margin/models/product_pricelist.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import logging
+
+from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
+class product_pricelist(models.Model):
+    _inherit = 'product.pricelist'
+
+    product_price_margin = fields.Float(
+        compute="get_product_margin",
+        string="Margin"
+    )
+
+    @api.depends('product_price')
+    @api.multi
+    def get_product_margin(self):
+        for rec in self:
+            rec._get_product_margin()
+
+    @api.multi
+    def _get_product_margin(self):
+        margin = 0.0
+        if self.product_id and self.product_price:
+            product = self.env['product.product'].search([('id', '=', self.product_id)])
+            if product:
+                if product.standard_price:
+                    margin = (self.product_price - product.standard_price) / self.product_price
+                else:
+                    margin = 1
+        self.product_price_margin = margin
+
