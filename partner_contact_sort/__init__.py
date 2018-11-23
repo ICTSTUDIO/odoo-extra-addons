aeaca9be111b1a970a70ae7f59cf39464bdc125b
André Schenkels <a.schenkels@ictstudio.eu>
2018-11-23 13:28:19 +0100

    [ADD] Partner Contact Sort Module

---
+++ partner_contact_sort/__init__.py
@@ -0,0 +1,3 @@
+# Copyright© 2016-2017 ICTSTUDIO <http://www.ictstudio.eu>
+# License: LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
---
+++ partner_contact_sort/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright© 2016-2017 ICTSTUDIO <http://www.ictstudio.eu>
+# License: LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+{
+    'name': 'Partner - Sort Contacts',
+    'version': '11.0.0.0.1',
+    'category': 'CRM',
+    'author': 'ICTSTUDIO | André Schenkels',
+    'website': 'http://www.ictstudio.eu',
+    'license': 'LGPL-3',
+    'summary': 'Sort Contacts in Partner view (type, name)',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+}
