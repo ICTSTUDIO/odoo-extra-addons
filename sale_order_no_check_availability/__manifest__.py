5ae295d0cd41015e9222241dbb4c33a47f72c22e
André Schenkels <a.schenkels@ictstudio.eu>
2018-11-23 11:26:27 +0100

    No Check Availability

---
+++ sale_order_no_check_availability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright© 2016-2017 ICTSTUDIO <http://www.ictstudio.eu>
+# License: LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+{
+    'name': 'Sale Order - No Check Availability',
+    'version': '11.0.0.0.1',
+    'category': 'Sale Management',
+    'author': 'ICTSTUDIO | André Schenkels',
+    'website': 'http://www.ictstudio.eu',
+    'license': 'LGPL-3',
+    'summary': 'No check availability popup when entering sale order lines',
+    'depends': [
+        'sale_stock',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
