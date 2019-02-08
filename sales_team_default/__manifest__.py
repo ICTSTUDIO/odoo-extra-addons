83be470f3932e90754065cd6fa926765a0563883
Tim Glimmerveen <tim@more2make.nl>
2019-02-08 16:33:14 +0100

    [ADD] Sales team default

---
+++ sales_team_default/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Sales Team - Default Base',
+    'version': '10.0.0.0.1',
+    'author': 'ICTSTUDIO, André Schenkels',
+    'license': 'AGPL-3',
+    'website': 'http://www.ictstudio.eu',
+    'category': 'Sale Management',
+    'depends': [
+        'sales_team'
+    ],
+    'data': [
+        'views/crm_case_section.xml'
+    ],
+}
